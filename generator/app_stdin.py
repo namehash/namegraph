4cf727a6d23a7d043e504f08bcc989b1d7da2f3b
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-01 22:39:37 +0200

    refactor stdin input

--- generator/app_stdin.py
+++ generator/app_stdin.py
@@ -1,14 +1,14 @@
 import sys
 
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from pathlib import Path
 import os
-
 from timeit import default_timer as timer
 from datetime import timedelta
+from typing import List
 
-from generator.generator import Generator
+from generator.xgenerator import Generator
 
 
 def generate_from_file(file):
@@ -18,14 +18,12 @@ def generate_from_file(file):
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
-def generate(config: DictConfig) -> None:
-    generator = Generator()
-    root = Path(os.path.abspath(__file__)).parents[1]
-    generator.read_domains(root / config.generator.domains)
+def generate(config: DictConfig):
+    generator = Generator(config)
 
     for query in generate_from_file(sys.stdin):
         start = timer()
-        suggestions = generator.generate_names(query, config.generator.suggestions)
+        suggestions = generator.generate_names(query, config.app.suggestions)
         end = timer()
         print(f"Generation time (s): {timedelta(seconds=end - start)}", file=sys.stderr)
         print(suggestions)
