d6ec19304639ff682353cb6483f03dbaa244f9e9
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2022-06-02 17:44:54 +0200

    Implement prefix generation

--- generator/generation/prefix_generator.py
+++ generator/generation/prefix_generator.py
@@ -2,14 +2,15 @@ from typing import List, Dict, Tuple
 
 from .name_generator import NameGenerator
 
+
 class PrefixGenerator(NameGenerator):
     """
     Add prefix.
     """
 
-    def __init__(self, prefixes):
+    def __init__(self, config):
         super().__init__()
-        self.prefixes = prefixes
+        self.prefixes = set([line.strip() for line in open(config.generation.prefixes_path)])
 
-    def generate(self, tokens: Tuple[str]) -> List[Tuple[str]]:
+    def generate(self, tokens: Tuple[str, ...]) -> List[Tuple[str, ...]]:
         return [tuple([prefix] + list(tokens)) for prefix in self.prefixes]
