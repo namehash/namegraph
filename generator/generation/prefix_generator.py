4d9e44c4f4a92208fead388f70a7d277875347cf
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-04 17:09:43 +0200

    do not add affix if a name has this affix

--- generator/generation/prefix_generator.py
+++ generator/generation/prefix_generator.py
@@ -13,4 +13,5 @@ class PrefixGenerator(NameGenerator):
         self.prefixes = set([line.strip() for line in open(config.generation.prefixes_path)])
 
     def generate(self, tokens: Tuple[str, ...]) -> List[Tuple[str, ...]]:
-        return [tuple([prefix] + list(tokens)) for prefix in self.prefixes]
+        name = ''.join(tokens)
+        return [tuple([prefix] + list(tokens)) for prefix in self.prefixes if not name.startswith(prefix)]
