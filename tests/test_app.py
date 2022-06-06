26e333b59c0c6089cca1dd5c1dae7a512e59d2a7
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2022-06-06 16:29:42 +0200

    Split conf into smaller parts (#17)

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,6 +20,19 @@ def test_basic_generation(overrides: List[str], expected: List[str]) -> None:
         result = generate(cfg, )[0]
         assert len(set(result).intersection(set(expected))) == len(expected)
 
+@pytest.mark.slow
+@mark.parametrize(
+    "overrides, expected",
+    [
+        (["app.query=firepower", "pipelines=full"], ["firepowercoin"]),
+    ],
+)
+def test_pipeline_override(overrides: List[str], expected: List[str]) -> None:
+    with initialize(version_base=None, config_path="../conf/"):
+        cfg = compose(config_name="test_config", overrides=overrides)
+        result = generate(cfg, )[0]
+        assert len(set(result).intersection(set(expected))) == len(expected)
+
 
 @mark.parametrize(
     "overrides, expected",
