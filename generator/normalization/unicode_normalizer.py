dc91c847244a1e494fd2d275e7ac99efd5d82e7b
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-07 15:48:46 +0200

    remove unused code

--- generator/normalization/unicode_normalizer.py
+++ generator/normalization/unicode_normalizer.py
@@ -6,5 +6,4 @@ class UnicodeNormalizer:
         pass
 
     def normalize(self, name: str) -> str:
-        # return unicodedata.normalize('NFKD', name).encode('ascii', 'ignore').decode('utf-8') # dont work with ł
         return unidecode(name, errors='ignore').lower()
