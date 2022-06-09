d0a7d004c81e99236cdbe7aaf060d4e6abf39d43
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-09 13:35:19 +0200

    add types

--- generator/domains.py
+++ generator/domains.py
@@ -7,9 +7,12 @@ from generator.filtering import SubnameFilter, ValidNameFilter
 
 class Domains:
     def __init__(self, config):
+        self.subname_filter = SubnameFilter(config)
+        self.validname_filter = ValidNameFilter(config)
+
         self.registered: Set[str] = self.read_txt(Path(config.filtering.root_path) / config.filtering.domains)
-        self.secondary_market: Dict[str, int] = self.read_json(config.app.secondary_market_names)
-        self.advertised: Dict[str, int] = self.read_json(config.app.advertised_names)
+        self.secondary_market: Dict[str, float] = self.read_json(config.app.secondary_market_names)
+        self.advertised: Dict[str, float] = self.read_json(config.app.advertised_names)
         self.internet: Set[str] = self.read_txt(config.app.internet_domains)
 
         for k in self.advertised:
@@ -21,11 +24,9 @@ class Domains:
         self.internet -= self.secondary_market.keys()
         self.internet -= self.advertised.keys()
 
-        subname_filter = SubnameFilter(config)
-        validname_filter = ValidNameFilter(config)
-        self.internet = set(validname_filter.apply(subname_filter.apply(self.internet)))
+        self.internet = set(self.validname_filter.apply(self.subname_filter.apply(self.internet)))
 
-    def read_txt(self, path):
+    def read_txt(self, path) -> Set[str]:
         domains: Set[str] = set()
         with open(path) as domains_file:
             for line in domains_file:
@@ -35,7 +36,11 @@ class Domains:
                 domains.add(domain)
         return domains
 
-    def read_json(self, path):
-        names = json.load(open(path))
-        names = {k[:-4] if k.endswith('.eth') else k: v for k, v in names.items()}
-        return names
+    def read_json(self, path) -> Dict[str, float]:
+        names_prices: Dict[str, float] = json.load(open(path))
+        names_prices = {(name[:-4] if name.endswith('.eth') else name): price for name, price in names_prices.items()}
+        names = self.subname_filter.apply(names_prices.keys())
+
+        result = {name: names_prices[name] for name in names}
+
+        return result
