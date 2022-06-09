9bec184a8bb7998c883b91cf5e259b1770505168
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-09 09:58:12 +0200

    test domain filtering

--- tests/test_domains.py
+++ tests/test_domains.py
@@ -2,7 +2,7 @@ from generator.domains import Domains
 from hydra import compose, initialize
 
 
-def test_domain_filter():
+def test_domains():
     with initialize(version_base=None, config_path="../conf/"):
         config = compose(config_name="test_config")
         domains = Domains(config)
@@ -10,3 +10,19 @@ def test_domain_filter():
         assert 'fire' in domains.secondary_market
         assert 'panda' in domains.advertised
         assert '00002' in domains.registered
+
+
+def test_domains_filtering():
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="test_config")
+        domains = Domains(config)
+        assert 'orange' in domains.advertised
+        assert 'global' in domains.advertised
+        assert 'orange' not in domains.secondary_market
+        assert 'global' not in domains.secondary_market
+        assert 'global' not in domains.internet
+        assert 'global' not in domains.registered
+        assert '00002' in domains.registered
+        assert '00002' not in domains.internet
+        assert '000' in domains.secondary_market
+        assert '002' not in domains.registered
