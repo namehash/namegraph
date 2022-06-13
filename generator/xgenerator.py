11081587f2604c01067f055a37665e83075fcb57
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-13 16:27:21 +0200

    simple multiprocessing - longer time execution

--- generator/xgenerator.py
+++ generator/xgenerator.py
@@ -1,4 +1,5 @@
-from itertools import zip_longest, chain
+from itertools import zip_longest, chain, repeat
+from multiprocessing import Pool
 from typing import List
 
 from generator.pipeline import Pipeline
@@ -20,8 +21,17 @@ class Generator():
     def generate_names(self, name: str, count: int) -> List[str]:
         suggestions = []
 
-        for pipeline in self.pipelines:
-            suggestions.append(pipeline.apply(name))
+        multiprocessing=self.config.app.use_multiprocessing
+        if multiprocessing:
+            with Pool(8) as pool:
+                tasks = zip([pipeline.apply for pipeline in self.pipelines], repeat([name]))
+                futures = [pool.apply_async(*t) for t in tasks]
+                results = [fut.get() for fut in futures]
+                suggestions=results
+        else:
+            for pipeline in self.pipelines:
+                pipeline_suggestions = pipeline.apply(name)
+                suggestions.append(pipeline_suggestions)
 
         combined_suggestions = list(by_one_iterator(suggestions))
         # TODO uniq
