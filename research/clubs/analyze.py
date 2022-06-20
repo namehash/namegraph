c659b0fa2852c18c8f736283ccc751f6dbe2f6c4
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-20 20:31:10 +0200

    analysis of regostered names in clubs

---
+++ research/clubs/analyze.py
@@ -0,0 +1,23 @@
+import random
+import re
+
+from generator.domains import Domains
+from generator.generation.categories_generator import load_categories
+from hydra import initialize, compose
+
+with initialize(version_base=None, config_path="../../conf/"):
+    config = compose(config_name="prod_config")
+    domains = Domains(config)
+    categories = load_categories(config)
+    registered = domains.registered | set(domains.secondary_market)
+    result = []
+    for category, names in categories.items():
+        left = set(names) - registered
+        result.append((len(left), len(names), category, len(left) / len(names)))
+
+    print('[unregistered names in the club] / [all in the club] = [ratio] [club name]: [sample of names]')
+    for left, all, category, percentage in sorted(result, reverse=True):
+        category_name = re.sub('^data/categories/', '', category)
+        names = categories[category]
+        random.shuffle(names)
+        print(f'{left} / {all} = {percentage:0.2f} {category_name}: {names[:5]}')
