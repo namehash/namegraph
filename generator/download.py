8bec945fe8d6e7423829505353451eb2661fe9f5
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-20 12:07:09 +0200

    fix input for namehash; enable w2v generator

--- generator/download.py
+++ generator/download.py
@@ -5,17 +5,15 @@ import gensim
 from generator.generation import W2VGenerator, WordnetSynonymsGenerator
 
 
-@hydra.main(version_base=None, config_path="../conf", config_name="config")
+@hydra.main(version_base=None, config_path="../conf", config_name="prod_config")
 def init(config: DictConfig):
     WordnetSynonymsGenerator(config)
     W2VGenerator(config)
-    # TODO convert embeddings to binary
 
+    # convert embeddings to binary
     model = api.load(config.generation.word2vec_model)
     model.save('data/embeddings.pkl')
     del model
-
-    # model = gensim.models.keyedvectors.KeyedVectors.load('data/embeddings.pkl')
 
     # TODO download files from S3
 
--- generator/xgenerator.py
+++ generator/xgenerator.py
@@ -1,7 +1,6 @@
 import logging
-import random
 from itertools import zip_longest, chain
-from typing import List, Dict, Tuple
+from typing import List, Dict
 
 from generator.domains import Domains
 from generator.pipeline import Pipeline
@@ -41,6 +40,34 @@ def uniq(l: List):
     return [x for x in l if x not in used and (used.add(x) or True)]
 
 
+class Result:
+    def __init__(self, config: DictConfig):
+        self.advertised = []
+        self.secondary = []
+        self.primary = []
+
+        self.domains = Domains(config)
+        self.suggestions = []
+
+    def add_pipeline_suggestions(self, pipeline_suggestions):
+        self.suggestions.append(pipeline_suggestions)
+
+    def split(self):
+        for pipeline_suggestions in self.suggestions:
+            advertised, remaining_suggestions = self.domains.get_advertised(pipeline_suggestions)
+            secondary, remaining_suggestions = self.domains.get_secondary(remaining_suggestions)
+            primary = self.domains.get_primary(remaining_suggestions)
+            self.advertised.append(advertised)
+            self.secondary.append(secondary)
+            self.primary.append(primary)
+
+    def combine(self):
+        advertised = list(by_one_iterator_uniq(self.advertised))
+        secondary = list(by_one_iterator_uniq(self.secondary))
+        primary = list(by_one_iterator_uniq(self.primary))
+        return advertised, secondary, primary
+
+
 class Generator():
     def __init__(self, config: DictConfig):
         self.config = config
@@ -49,24 +76,18 @@ class Generator():
         for definition in self.config.pipelines:
             self.pipelines.append(Pipeline(definition, self.config))
 
-        self.domains = Domains(config)
-
     def generate_names(self, name: str) -> Dict[str, List[str]]:
         count = self.config.app.suggestions
-        suggestions = []
+
+        result = Result(self.config)
 
         for pipeline in self.pipelines:
             pipeline_suggestions = pipeline.apply(name)
             logger.debug(f'Pipeline suggestions: {pipeline_suggestions[:10]}')
-            suggestions.append(pipeline_suggestions)
+            result.add_pipeline_suggestions(pipeline_suggestions)
 
-        combined_suggestions = list(by_one_iterator_uniq(suggestions))
-
-        # combined_suggestions = uniq(combined_suggestions)
-
-        advertised, remaining_suggestions = self.domains.get_advertised(combined_suggestions)
-        secondary, remaining_suggestions = self.domains.get_secondary(remaining_suggestions)
-        primary = self.domains.get_primary(remaining_suggestions)
+        result.split()
+        advertised, secondary, primary = result.combine()
 
         results = {'advertised': advertised[:count],
                    'secondary': secondary[:count],
