59c14fa1560fcc02efd8349e63d1fea9976ed669
Krzysztof Wróbel <djstrong@gmail.com>
2022-06-20 12:43:26 +0200

    fix CI

--- generator/download.py
+++ generator/download.py
@@ -8,7 +8,6 @@ from generator.generation import W2VGenerator, WordnetSynonymsGenerator
 @hydra.main(version_base=None, config_path="../conf", config_name="prod_config")
 def init(config: DictConfig):
     WordnetSynonymsGenerator(config)
-    W2VGenerator(config)
 
     # convert embeddings to binary
     model = api.load(config.generation.word2vec_model)
