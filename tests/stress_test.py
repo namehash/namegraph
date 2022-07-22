6359bc4fe2f63b053004c6af4d110fe81b0f054e
Carbon225 <carbon225@protonmail.com>
2022-07-22 13:08:44 +0200

    stress test args and simple filter

--- tests/stress_test.py
+++ tests/stress_test.py
@@ -1,12 +1,12 @@
 import sys
 import os
+import math
 from time import time as get_time
+import argparse
+
 from generator.domains import Domains
 from fastapi.testclient import TestClient
-from helpers import check_inspector_response, check_generator_response
-
-
-TIME_LIMIT = 1
+from helpers import check_inspector_response, check_generator_response, SPECIAL_CHAR_REGEX
 
 
 def prod_test_client():
@@ -44,31 +44,39 @@ def stress_test(fn, filename):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3 or sys.argv[1] not in ['inspector', 'generator']:
-        print(f'Usage: python {sys.argv[0]} inspector|generator <data file>')
-        print(f'Example: python {sys.argv[0]} inspector tests/data/primary.csv')
-        exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no-simple', action='store_true', help='Skip simple names')
+    parser.add_argument('-t', type=float, default=1, help='Timeout [s] for each request (0 for no timeout)')
+    parser.add_argument('module', choices=['inspector', 'generator'], help='Module to test')
+    parser.add_argument('data_file', help='File with names to use')
+    args = parser.parse_args()
 
-    module = sys.argv[1]
-    filename = sys.argv[2]
+    module = args.module
+    filename = args.data_file
+    timeout = math.inf if args.t == 0 else args.t
+    enable_filter = args.no_simple
 
     print('Creating client...')
     client = prod_test_client()
 
     def run_inspector(name):
+        if enable_filter and SPECIAL_CHAR_REGEX.search(name) is None:
+            return
         start = get_time()
         resp = client.post('/inspector/', json={'name': name})
         duration = get_time() - start
         assert resp.status_code == 200
-        assert duration < TIME_LIMIT, f'Time limit exceeded'
+        assert duration < timeout, f'Time limit exceeded'
         check_inspector_response(name, resp.json())
 
     def run_generator(name):
+        if enable_filter and SPECIAL_CHAR_REGEX.search(name) is None:
+            return
         start = get_time()
         resp = client.post('/', json={'name': name})
         duration = get_time() - start
         assert resp.status_code == 200
-        assert duration < TIME_LIMIT, f'Time limit exceeded'
+        assert duration < timeout, f'Time limit exceeded'
         check_generator_response(resp.json())
 
     if module == 'inspector':
