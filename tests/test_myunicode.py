d6bd03b45bf7282a257e71f85be67c4df35806f6
Carbon225 <carbon225@protonmail.com>
2022-07-23 13:41:51 +0200

    [myunicode] more tests

--- tests/test_myunicode.py
+++ tests/test_myunicode.py
@@ -3,34 +3,116 @@ import pytest
 import myunicode
 
 
-def test_name():
-    assert myunicode.name('ア') == 'KATAKANA LETTER A'
+@pytest.mark.parametrize(
+    'chr,expected',
+    [
+        ('a', 'LATIN SMALL LETTER A'),
+        ('ア', 'KATAKANA LETTER A'),
+        ('ἀ', 'GREEK SMALL LETTER ALPHA WITH PSILI'),
+        ('יִ', 'HEBREW LETTER YOD WITH HIRIQ'),
+        ('\u3099', 'COMBINING KATAKANA-HIRAGANA VOICED SOUND MARK'),
+    ]
+)
+def test_name(chr, expected):
+    assert myunicode.name(chr) == expected
+    assert myunicode.name(chr, 'default') == expected
 
 
-def test_name_default():
-    assert myunicode.name('🩶', default='default') == 'default'
+@pytest.mark.parametrize(
+    'chr,expected',
+    [
+        ('a', 'LATIN SMALL LETTER A'),
+        ('🩶', 'default'),
+    ]
+)
+def test_name_default(chr, expected):
+    assert myunicode.name(chr, default='default') == expected
 
 
-def test_name_throws_on_missing():
-    with pytest.raises(ValueError):
-        myunicode.name('🩶')
+@pytest.mark.parametrize('chr', ['🩶'])
+def test_name_throws_on_missing(chr):
+    with pytest.raises(ValueError, match='no such name'):
+        myunicode.name(chr)
 
 
-def test_category():
-    assert myunicode.category('ア') == 'Lo'
+@pytest.mark.parametrize('chr', ['abc', 'aア', ''])
+def test_name_throws_on_str(chr):
+    with pytest.raises(TypeError, match=r'name\(\) argument 1 must be a unicode character, not str'):
+        myunicode.name(chr)
+    with pytest.raises(TypeError, match=r'name\(\) argument 1 must be a unicode character, not str'):
+        myunicode.name(chr, 'default')
 
 
-def test_combining():
-    assert myunicode.combining('\u3099') == 8
+@pytest.mark.parametrize(
+    'chr,expected',
+    [
+        ('a', 'Ll'),
+        ('ア', 'Lo'),
+        ('\u0600', 'Cf'),
+        ('🩶', 'Cn'),
+    ]
+)
+def test_category(chr, expected):
+    assert myunicode.category(chr) == expected
 
 
-def test_block_of():
-    assert myunicode.block_of('ア') == 'KATAKANA'
+@pytest.mark.parametrize('chr', ['abc', 'aア', ''])
+def test_category_throws_on_str(chr):
+    with pytest.raises(TypeError, match=r'category\(\) argument must be a unicode character, not str'):
+        myunicode.category(chr)
 
 
-def test_script_of():
-    assert False
+@pytest.mark.parametrize(
+    'chr,expected',
+    [
+        ('a', 0),
+        ('🩶', 0),
+        ('\u3099', 8),
+    ]
+)
+def test_combining(chr, expected):
+    assert myunicode.combining(chr) == expected
 
 
-def test_emojis():
-    assert '🫶' in myunicode.EMOJIS
+@pytest.mark.parametrize('chr', ['abc', 'aア', ''])
+def test_combining_throws_on_str(chr):
+    with pytest.raises(TypeError, match=r'combining\(\) argument must be a unicode character, not str'):
+        myunicode.combining(chr)
+
+
+@pytest.mark.parametrize(
+    'chr,expected',
+    [
+        ('a', 'BASIC_LATIN'),
+        ('ア', 'KATAKANA'),
+        ('🩶', None)
+    ]
+)
+def test_block_of(chr, expected):
+    assert myunicode.block_of(chr) == expected
+
+
+@pytest.mark.parametrize('chr', ['abc', 'aア', ''])
+def test_block_of_throws_on_str(chr):
+    with pytest.raises(Exception, match=r'block_of\(\) argument must be a unicode character, not str'):
+        myunicode.block_of(chr)
+
+
+@pytest.mark.parametrize(
+    'chr,expected',
+    [
+        ('יִ', 'Hebrew'),
+        ('a', 'Latin'),
+        ('abc', 'Latin'),
+        ('ア', 'Katakana'),
+        ('アア', 'Katakana'),
+        ('aア', None),
+    ]
+)
+def test_script_of(chr, expected):
+    assert myunicode.script_of(chr) == expected
+
+
+@pytest.mark.parametrize('emoji', ['🫶'])
+def test_emojis(emoji):
+    assert emoji in myunicode.EMOJIS
