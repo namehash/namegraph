b89e3b5c7b92b9648124fd7a944a9139009525c2
Carbon225 <carbon225@protonmail.com>
2022-07-23 18:10:59 +0200

    [myunicode] scripts

--- myunicode/download.py
+++ myunicode/download.py
@@ -6,7 +6,11 @@ import regex
 _PATH = 'data/myunicode'
 
 # e.g. 11AB0..11ABF; Unified Canadian Aboriginal Syllabics Extended-A
-_BLOCK_REGEX = regex.compile(r'(?<start>[0-9a-fA-F]*)..(?<stop>[0-9a-fA-F]*);(?<name>.*)')
+_BLOCK_REGEX = regex.compile(r'^(?<start>[0-9a-fA-F]+)..(?<stop>[0-9a-fA-F]+)\s*;(?<name>.+)$')
+
+# e.g. 0000..001F    ; Common # Cc  [32] <control-0000>..<control-001F>
+# or   0020          ; Common # Zs       SPACE
+_SCRIPT_REGEX = regex.compile(r'^(?<start>[0-9a-fA-F]+)(?:..(?<stop>[0-9a-fA-F]+))?\s*;(?<script>[^#]+)#.*$')
 
 
 def download():
@@ -43,6 +47,39 @@ def download():
             name = m.group('name').strip()
 
             f.write(f'{start};{stop};{name}\n')
+
+    r = requests.get('https://www.unicode.org/Public/UNIDATA/Scripts.txt')
+    scripts = []
+    for line in r.text.splitlines():
+        line = line.strip()
+
+        # Skip comments and empty lines
+        if len(line) == 0 or line[0] == '#':
+            continue
+
+        m = _SCRIPT_REGEX.search(line)
+        start = int(m.group('start'), base=16)
+        stop = int(m.group('stop'), base=16) if m.group('stop') is not None else start
+        script = m.group('script').strip()
+
+        scripts.append((start, stop, script))
+
+    scripts.sort(key=lambda x: x[0])
+
+    # compress continuous script ranges
+    compressed = []
+    for s in scripts:
+        # if current script range extends the previous one
+        if len(compressed) > 0 and s[0] == compressed[-1][1] + 1 and s[2] == compressed[-1][2]:
+            # extend last range
+            compressed[-1] = (compressed[-1][0], s[1], compressed[-1][2])
+        else:
+            # add new range
+            compressed.append(s)
+
+    with open(f'{_PATH}/Scripts.txt', 'w') as f:
+        for s in compressed:
+            f.write(f'{s[0]:04X};{s[1]:04X};{s[2]}\n')
 
 
 if __name__ == "__main__":
