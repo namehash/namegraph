c409f9cccafb0f41499c30e95e6a029a0fe4a454
Carbon225 <carbon225@protonmail.com>
2022-07-23 13:58:27 +0200

    [myunicode] optimize data loading

--- myunicode/data.py
+++ myunicode/data.py
@@ -1,29 +1,31 @@
 def _load_data():
+    from typing import Iterable, Tuple
+
+    def make_entry(line: str) -> Tuple[int, Tuple[str, str, int]]:
+        fields = line.split(';')
+
+        code = int(fields[0], base=16)
+        name = fields[1]
+        category = fields[2]
+        combining = int(fields[3])
+
+        return code, (name, category, combining)
+
     class UnicodeData:
-        def __init__(self):
-            self.name = {}
-            self.category = {}
-            self.combining = {}
+        def __init__(self, lines: Iterable[str]):
+            self.data = {e[0]: e[1] for e in map(make_entry, lines)}
 
-        def push(self, line: str):
-            fields = line.split(';')
+        def name(self, code: int) -> str:
+            return self.data[code][0]
 
-            code = int(fields[0], base=16)
-            name = fields[1]
-            category = fields[2]
-            combining = int(fields[3])
+        def category(self, code: int) -> str:
+            return self.data[code][1]
 
-            self.name[code] = name
-            self.category[code] = category
-            self.combining[code] = combining
-
-    data = UnicodeData()
+        def combining(self, code: int) -> int:
+            return self.data[code][2]
 
     with open('data/myunicode/UnicodeData.txt', 'r') as f:
-        for line in f:
-            data.push(line)
-
-    return data
+        return UnicodeData(f)
 
 
 UNICODE_DATA = _load_data()
--- myunicode/myunicode.py
+++ myunicode/myunicode.py
@@ -4,25 +4,30 @@ from .data import UNICODE_DATA
 def name(chr: str, default=None) -> str:
     if len(chr) != 1:
         raise TypeError('name() argument 1 must be a unicode character, not str')
-    if default is None:
-        try:
-            return UNICODE_DATA.name[ord(chr)]
-        except KeyError:
+    try:
+        return UNICODE_DATA.name(ord(chr))
+    except KeyError:
+        if default is None:
             raise ValueError('no such name')
-    else:
-        return UNICODE_DATA.name.get(ord(chr), default)
+        return default
 
 
 def category(chr: str) -> str:
     if len(chr) != 1:
         raise TypeError('category() argument must be a unicode character, not str')
-    return UNICODE_DATA.category.get(ord(chr), 'Cn')
+    try:
+        return UNICODE_DATA.category(ord(chr))
+    except KeyError:
+        return 'Cn'
 
 
 def combining(chr: str) -> int:
     if len(chr) != 1:
         raise TypeError('combining() argument must be a unicode character, not str')
-    return UNICODE_DATA.combining.get(ord(chr), 0)
+    try:
+        return UNICODE_DATA.combining(ord(chr))
+    except KeyError:
+        return 0
 
 
 def block_of(chr: str) -> str:
