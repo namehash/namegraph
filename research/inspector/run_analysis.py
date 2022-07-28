bd2ab78a7ab40bf60fbf361f2deabbc769ab654b
Carbon225 <carbon225@protonmail.com>
2022-07-28 08:36:03 +0200

    [inspector flag analysis] update to new api

--- research/inspector/run_analysis.py
+++ research/inspector/run_analysis.py
@@ -35,10 +35,10 @@ client = create_client()
 
 
 def run_test(
-    name: str,
-    score: bool,
+    label: str,
+    tokenization: bool,
     limit_confusables: bool,
-    disable_chars_output: bool,
+    truncate_chars_output: int,
     disable_char_analysis: bool,
 ):
     durations = []
@@ -46,10 +46,10 @@ def run_test(
     for _ in range(5):
         start = get_time()
         resp = client.post('/inspector/', json={
-            'name': name,
-            'score': score,
+            'label': label,
+            'tokenization': tokenization,
             'limit_confusables': limit_confusables,
-            'disable_chars_output': disable_chars_output,
+            'truncate_chars_output': truncate_chars_output,
             'disable_char_analysis': disable_char_analysis,
             })
         duration = get_time() - start
@@ -62,100 +62,100 @@ def run_test(
 
 
 default_results = []
-no_score_results = []
+no_tokenization_results = []
 limit_confusables_results = []
-disable_chars_output_results = []
+truncate_chars_output_results = []
 disable_char_analysis_results = []
-no_score_limit_confusables_results = []
-no_score_disable_chars_output_results = []
+no_tokenization_limit_confusables_results = []
+no_tokenization_truncate_chars_output_results = []
 
 lengths = range(10_000, 40_001, 10_000)
 
 print('Running tests...')
 for l in tqdm(lengths):
     name = 'a' * l + '.eth'
-    default_results.append(                       run_test(name, True,  False, False, False))
-    no_score_results.append(                      run_test(name, False, False, False, False))
-    limit_confusables_results.append(             run_test(name, True,  True,  False, False))
-    disable_chars_output_results.append(          run_test(name, True,  False, True,  False))
-    disable_char_analysis_results.append(         run_test(name, True,  False, False, True))
-    no_score_limit_confusables_results.append(    run_test(name, False, True,  False, False))
-    no_score_disable_chars_output_results.append( run_test(name, False, False, True,  False))
+    default_results.append(                              run_test(name, True,  False, None, False))
+    no_tokenization_results.append(                      run_test(name, False, False, None, False))
+    limit_confusables_results.append(                    run_test(name, True,  True,  None, False))
+    truncate_chars_output_results.append(                run_test(name, True,  False, 0,    False))
+    disable_char_analysis_results.append(                run_test(name, True,  False, None, True))
+    no_tokenization_limit_confusables_results.append(    run_test(name, False, True,  None, False))
+    no_tokenization_truncate_chars_output_results.append(run_test(name, False, False, 0,    False))
 
 
 print('Drawing plots...')
 
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in default_results], label='default')
+plt.plot(lengths, [r[0] for r in default_results])
 plt.title('Duration - default')
 plt.savefig(THIS_DIR / 'duration_default.png')
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in no_score_results], label='no score')
-plt.title('Duration - no score')
-plt.savefig(THIS_DIR / 'duration_no_score.png')
+plt.plot(lengths, [r[0] for r in no_tokenization_results])
+plt.title('Duration - no tokenization')
+plt.savefig(THIS_DIR / 'duration_no_tokenization.png')
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in limit_confusables_results], label='limit confusables')
+plt.plot(lengths, [r[0] for r in limit_confusables_results])
 plt.title('Duration - limit confusables')
 plt.savefig(THIS_DIR / 'duration_limit_confusables.png')
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in disable_chars_output_results], label='disable chars output')
-plt.title('Duration - disable chars output')
-plt.savefig(THIS_DIR / 'duration_disable_chars_output.png')
+plt.plot(lengths, [r[0] for r in truncate_chars_output_results])
+plt.title('Duration - truncate chars output')
+plt.savefig(THIS_DIR / 'duration_truncate_chars_output.png')
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in disable_char_analysis_results], label='disable char analysis')
+plt.plot(lengths, [r[0] for r in disable_char_analysis_results])
 plt.title('Duration - disable char analysis')
 plt.savefig(THIS_DIR / 'duration_disable_char_analysis.png')
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in no_score_limit_confusables_results], label='no score limit confusables')
-plt.title('Duration - no score limit confusables')
-plt.savefig(THIS_DIR / 'duration_no_score_limit_confusables.png')
+plt.plot(lengths, [r[0] for r in no_tokenization_limit_confusables_results])
+plt.title('Duration - no tokenization limit confusables')
+plt.savefig(THIS_DIR / 'duration_no_tokenization_limit_confusables.png')
 
 plt.clf()
-plt.plot(lengths, [r[0] for r in no_score_disable_chars_output_results], label='no score disable chars output')
-plt.title('Duration - no score disable chars output')
-plt.savefig(THIS_DIR / 'duration_no_score_disable_chars_output.png')
+plt.plot(lengths, [r[0] for r in no_tokenization_truncate_chars_output_results])
+plt.title('Duration - no tokenization truncate chars output')
+plt.savefig(THIS_DIR / 'duration_no_tokenization_truncate_chars_output.png')
 
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in default_results], label='default')
+plt.plot(lengths, [r[1] for r in default_results])
 plt.title('Size - default')
 plt.savefig(THIS_DIR / 'size_default.png')
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in no_score_results], label='no score')
-plt.title('Size - no score')
-plt.savefig(THIS_DIR / 'size_no_score.png')
+plt.plot(lengths, [r[1] for r in no_tokenization_results])
+plt.title('Size - no tokenization')
+plt.savefig(THIS_DIR / 'size_no_tokenization.png')
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in limit_confusables_results], label='limit confusables')
+plt.plot(lengths, [r[1] for r in limit_confusables_results])
 plt.title('Size - limit confusables')
 plt.savefig(THIS_DIR / 'size_limit_confusables.png')
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in disable_chars_output_results], label='disable chars output')
-plt.title('Size - disable chars output')
-plt.savefig(THIS_DIR / 'size_disable_chars_output.png')
+plt.plot(lengths, [r[1] for r in truncate_chars_output_results])
+plt.title('Size - truncate chars output')
+plt.savefig(THIS_DIR / 'size_truncate_chars_output.png')
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in disable_char_analysis_results], label='disable char analysis')
+plt.plot(lengths, [r[1] for r in disable_char_analysis_results])
 plt.title('Size - disable char analysis')
 plt.savefig(THIS_DIR / 'size_disable_char_analysis.png')
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in no_score_limit_confusables_results], label='no score limit confusables')
-plt.title('Size - no score limit confusables')
-plt.savefig(THIS_DIR / 'size_no_score_limit_confusables.png')
+plt.plot(lengths, [r[1] for r in no_tokenization_limit_confusables_results])
+plt.title('Size - no tokenization limit confusables')
+plt.savefig(THIS_DIR / 'size_no_tokenization_limit_confusables.png')
 
 plt.clf()
-plt.plot(lengths, [r[1] for r in no_score_disable_chars_output_results], label='no score disable chars output')
-plt.title('Size - no score disable chars output')
-plt.savefig(THIS_DIR / 'size_no_score_disable_chars_output.png')
+plt.plot(lengths, [r[1] for r in no_tokenization_truncate_chars_output_results])
+plt.title('Size - no tokenization truncate chars output')
+plt.savefig(THIS_DIR / 'size_no_tokenization_truncate_chars_output.png')
 
 
 print('Done')
