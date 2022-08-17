d884b5f9dfcf1ba138295f76ac7803baa87fc07f
Mykola Haltiuk <kaljandlux@gmail.com>
2022-08-17 16:15:26 +0200

    added tests for metadata (what about merging results from different pipelines?)

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -37,3 +37,69 @@ def test_duplicates_in_suggestions(overrides: List[str]) -> None:
         words = [str(name) for name in result]
 
         assert len(set(words)) == len(result)
+
+
+@mark.parametrize(
+    "overrides, pipeline_id, expected_strategies",
+    [
+        (
+            ["app.query=firepower", "app.suggestions=1000"],
+            0,
+            [[
+                "StripEthNormalizer", "UnicodeNormalizer", "NamehashNormalizer", "ReplaceInvalidNormalizer",
+                "LongNameNormalizer", "BigramWordnetTokenizer", "WordnetSynonymsGenerator", "SubnameFilter",
+                "ValidNameFilter"
+            ]]
+        ),
+        (
+            ["app.query=firepower", "app.suggestions=1000"],
+            1,
+            [[
+                "StripEthNormalizer", "UnicodeNormalizer", "NamehashNormalizer", "ReplaceInvalidNormalizer",
+                "LongNameNormalizer", "WordNinjaTokenizer", "PermuteGenerator", "SubnameFilter",
+                "ValidNameFilter"
+            ]]
+        )
+    ]
+)
+def test_metadata(overrides: List[str], pipeline_id: int, expected_strategies: List[str]) -> None:
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="test_config", overrides=overrides)
+        pipeline = Pipeline(config.pipelines[pipeline_id], config)
+        result = pipeline.apply(config.app.query)
+
+        for gn in result:
+            assert gn.applied_strategies == expected_strategies
+
+
+@mark.parametrize(
+    "overrides, pipeline_id, expected_strategies",
+    [
+        (
+            ["app.query=dogcatdog", "app.suggestions=1000"],
+            0,
+            [[
+                "StripEthNormalizer", "UnicodeNormalizer", "NamehashNormalizer", "ReplaceInvalidNormalizer",
+                "LongNameNormalizer", "BigramWordnetTokenizer", "WordnetSynonymsGenerator", "SubnameFilter",
+                "ValidNameFilter"
+            ]]
+        ),
+        (
+            ["app.query=dogcatdog", "app.suggestions=1000"],
+            1,
+            [[
+                "StripEthNormalizer", "UnicodeNormalizer", "NamehashNormalizer", "ReplaceInvalidNormalizer",
+                "LongNameNormalizer", "WordNinjaTokenizer", "PermuteGenerator", "SubnameFilter",
+                "ValidNameFilter"
+            ]] * 2  # duplicate
+        )
+    ]
+)
+def test_metadata_aggregation(overrides: List[str], pipeline_id: int, expected_strategies: List[str]) -> None:
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="test_config", overrides=overrides)
+        pipeline = Pipeline(config.pipelines[pipeline_id], config)
+        result = pipeline.apply(config.app.query)
+
+        for gn in result:
+            assert gn.applied_strategies == expected_strategies
