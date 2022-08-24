a276f691eb924ce257c08dd5116481ccd372a697
Mykola Haltiuk <kaljandlux@gmail.com>
2022-08-24 14:20:09 +0200

    added constraints using generation.limit

--- generator/generation/random_generator.py
+++ generator/generation/random_generator.py
@@ -1,8 +1,10 @@
-import random
+import random, logging
 from typing import List, Tuple
 
 from .name_generator import NameGenerator
 from ..domains import Domains
+
+logger = logging.getLogger('generator')
 
 
 class RandomGenerator(NameGenerator):
@@ -13,12 +15,15 @@ class RandomGenerator(NameGenerator):
     def __init__(self, config):
         super().__init__()
         self.config = config
+        self.limit = self.config.generation.limit * 2
         self.domains = Domains(config)
 
+        if len(self.domains.internet) < self.limit:
+            logger.warning('the number of internet domains for random generator is smaller than the generation limit')
+
     def generate(self, tokens: Tuple[str, ...]) -> List[Tuple[str, ...]]:
-        count = self.config.app.suggestions * 2
-        if len(self.domains.internet) >= count:
-            result = random.sample(self.domains.internet, count)
+        if len(self.domains.internet) >= self.limit:
+            result = random.sample(self.domains.internet, self.limit)
         else:
             result = self.domains.internet
         return [(x,) for x in result]
--- generator/xgenerator.py
+++ generator/xgenerator.py
@@ -96,7 +96,7 @@ class Generator():
         result.split()
         advertised, secondary, primary = result.combine(sorter)
 
-        while len(primary) < min_suggestions:
+        if len(primary) < min_suggestions:
             # generate using random pipeline
             logger.debug('Generate random')
             random_suggestions = self.random_pipeline.apply(name)
@@ -105,7 +105,8 @@ class Generator():
             result_random.add_pipeline_suggestions(random_suggestions)
             result_random.split()
             _, _, random_names = result_random.combine(sorter)
-            primary = sorter.sort([aggregate_duplicates(primary + random_names)])
+            # TODO do we need to truncate the random suggestions before sorting?
+            primary = sorter.sort([aggregate_duplicates(primary + random_names)[:min_suggestions]])
 
         results = {'advertised': advertised[:max_suggestions],
                    'secondary': secondary[:max_suggestions],
