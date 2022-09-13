fixed weights for softmax, added more tests, aggregation inplace

generator/utils/math.py
@@ -1,7 +1,8 @@
 import numpy as np
+import numpy.typing as npt
 
 
-def softmax(x: np.ndarray):
+def softmax(x: npt.NDArray[np.float]):
     exps = np.exp(x - np.amax(x))
     exps_totals = np.sum(exps)
     return exps / exps_totals