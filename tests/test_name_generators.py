905832f49fd70a941f2387ae47ada5322ed59430
Krzysztof Wróbel <djstrong@gmail.com>
2022-11-09 16:24:33 +0100

    hyphen and abbrev long tests

--- tests/test_name_generators.py
+++ tests/test_name_generators.py
@@ -186,6 +186,22 @@ def test_hyphen_generator():
         assert ('my', '-', 'pikachu', '123') in [x.tokens for x in generated_names]
         assert ('my', 'pikachu', '123') not in [x.tokens for x in generated_names]
 
+@pytest.mark.execution_timeout(1)
+def test_hyphen_generator_long():
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="test_config")
+        strategy = HyphenGenerator(config)
+        tokenized_name = GeneratedName(tuple(['a']*10000))
+        generated_names = strategy.apply([tokenized_name])
+
+@pytest.mark.execution_timeout(1)
+def test_abbreviation_generator_long():
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="test_config")
+        strategy = AbbreviationGenerator(config)
+        tokenized_name = GeneratedName(tuple(['a']*10000))
+        generated_names = strategy.apply([tokenized_name])
+        
 
 def test_flag_generator():
     with initialize(version_base=None, config_path="../conf/"):
