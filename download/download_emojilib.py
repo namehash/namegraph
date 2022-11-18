17f873fb8c6f09b8b5fafac7757b781d7df15049
Mykola Haltiuk <kaljandlux@gmail.com>
2022-11-18 20:13:50 +0100

    emoji synonyms

--- download/download_emojilib.py
+++ download/download_emojilib.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import json
 import re
 
+import numpy as np
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -11,25 +12,71 @@ import requests
 
 PROJECT_ROOT_DIR = Path(__name__).parent.parent
 TOKEN_SEPARATOR = re.compile(r'[_ ]')
+STOPWORDS = set(stopwords.words('english'))
 SKIPPED_STOPWORDS = ['*', '#']
 
 
-if __name__ == '__main__':
+def download_emoji2names() -> dict[str, list[str]]:
     r = requests.get(f'https://raw.githubusercontent.com/muan/emojilib/main/dist/emoji-en-US.json')
     json_data = json.loads(r.text)
-    STOPWORDS = set(stopwords.words('english'))
+    return json_data
 
-    name2emoji = defaultdict(dict)  # dict to preserve order
-    for emoji, names in json_data.items():
+
+def normalize_names(emoji2names: dict[str, list[str]]) -> dict[str, list[str]]:
+    emoji2normalized_names = dict()
+    for emoji, names in emoji2names.items():
+        normalized_names = []
         for name in names:
             for token in TOKEN_SEPARATOR.split(name):
                 if not token or token in STOPWORDS and token not in SKIPPED_STOPWORDS:
                     continue
-                name2emoji[token][emoji] = None
 
-    name2emoji = {token: list(emojis.keys()) for token, emojis in name2emoji.items()}
+                normalized_names.append(name)
 
-    print(list(name2emoji.keys()))
+        emoji2normalized_names[emoji] = normalized_names
+    return emoji2normalized_names
+
+
+def enhance_names(emoji2names: dict[str, list[str]], threshold: float = 0.5) -> dict[str, list[str]]:
+    from gensim.models.keyedvectors import KeyedVectors
+
+    model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    model.init_sims(replace=True)
+
+    emoji2enhanced_names = dict()
+    for emoji, names in emoji2names.items():
+        enhanced_names = []
+        for name in names:
+            if name not in model:
+                enhanced_names.append(name)
+            else:
+                similarities = model.vectors @ model[name]
+                indices, = np.where(similarities > threshold)
+                best_similarities = similarities[indices]
+                sorted_indices = indices[np.argsort(best_similarities)[::-1]]
+
+                for index in sorted_indices:
+                    enhanced_names.append(model.index_to_key[index])
+                    print(f'GENERATING {model.index_to_key[index]} AS A SYNONYM TO {name}')
+
+        emoji2enhanced_names[emoji] = enhanced_names
+
+    return emoji2enhanced_names
+
+
+def invert_emoji2names_mapping(emoji2names: dict[str, list[str]]) -> dict[str, list[str]]:
+    name2emojis = defaultdict(list)
+    for emoji, names in emoji2names.items():
+        for name in names:
+            name2emojis[name].append(emoji)
+    return dict(name2emojis)
+
+
+if __name__ == '__main__':
+    emoji2names = download_emoji2names()
+    emoji2names_normalized = normalize_names(emoji2names)
+    emoji2names_enhanced = enhance_names(emoji2names_normalized)
+    name2emojis = invert_emoji2names_mapping(emoji2names_enhanced)
 
     with open(PROJECT_ROOT_DIR / 'data' / 'name2emoji.json', 'w', encoding='utf-8') as f:
-        json.dump(name2emoji, f, indent=2, ensure_ascii=False)
+        json.dump(name2emojis, f, indent=2, ensure_ascii=False)
