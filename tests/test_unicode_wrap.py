3889c710858492412e14f19f424a0c1569c32176
Carbon225 <carbon225@protonmail.com>
2022-12-13 04:02:14 +0100

    update unicode wrap

--- tests/test_unicode_wrap.py
+++ tests/test_unicode_wrap.py
@@ -5,6 +5,7 @@ def test_wrap():
     assert unicode_wrap('foo bar') == 'foo bar'
     assert unicode_wrap('fóó bar') == 'f(243)(243) bar'
     assert unicode_wrap('fóó 0 bar') == 'f(243)(243) (48) bar'
+    assert unicode_wrap('(1)') == '(40)(49)(41)'
 
 
 def test_empty():
