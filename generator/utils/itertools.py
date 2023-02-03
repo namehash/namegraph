15ea41d79b7dd2d30d697273e40a02555cb8f457
Krzysztof Wróbel <djstrong@gmail.com>
2023-02-03 23:26:20 +0100

    is it correct?

--- generator/utils/itertools.py
+++ generator/utils/itertools.py
@@ -1,7 +1,5 @@
 from typing import Iterable, List, Dict
-from collections import defaultdict
 
 
 def sort_by_value(items: Iterable[str], scores: Dict[str, float], reverse: bool = False) -> List[str]:
-    scores = defaultdict(float, scores)
-    return sorted(items, key=scores.get, reverse=reverse)
+    return sorted(items, key=lambda x: scores.get(x, 0.0), reverse=reverse)
