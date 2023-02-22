376f162308a4644eb2525c7bfa4b513cbd127b3b
Krzysztof Wróbel <djstrong@gmail.com>
2023-02-22 16:38:37 +0100

    remove sorters and tests for samplers

--- generator/sorting/__init__.py
+++
@@ -1,4 +0,0 @@
-from .count_sorter import CountSorter
-from .round_robin_sorter import RoundRobinSorter
-from .length_sorter import LengthSorter
-from .weighted_sampling_sorter import WeightedSamplingSorter
