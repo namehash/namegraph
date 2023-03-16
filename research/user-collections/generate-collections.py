2f42746eb105884dfc0203d164aede0ccec2024d
Krzysztof Wróbel <djstrong@gmail.com>
2023-03-16 23:48:13 +0100

    collections filtered with types

--- research/user-collections/generate-collections.py
+++ research/user-collections/generate-collections.py
@@ -4,6 +4,7 @@ import jsonlines
 
 import tqdm
 import wikipediaapi
+from SPARQLWrapper import SPARQLWrapper, JSON
 from wikidata.client import Client
 
 wikidata = Client()
@@ -11,9 +12,10 @@ wiki_wiki = wikipediaapi.Wikipedia(
     language='en',
     extract_format=wikipediaapi.ExtractFormat.WIKI
 )
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
 
-def category_members(category_name: str) -> list:
+def category_members2(category_name: str) -> list:
     members = set()
 
     category = wiki_wiki.page(category_name, unquote=True)
@@ -24,6 +26,29 @@ def category_members(category_name: str) -> list:
             members.add(member.title)  # TODO do we need anything else apart from title?
 
     return list(members)
+
+
+def category_members(category_name: str, type: str) -> list:
+    a = """SELECT ?item ?itemLabel ?type WHERE {{
+      SERVICE wikibase:mwapi {{
+         bd:serviceParam wikibase:api "Generator" .
+         bd:serviceParam wikibase:endpoint "en.wikipedia.org" .
+         bd:serviceParam mwapi:gcmtitle '{category_name}' .
+         bd:serviceParam mwapi:generator "categorymembers" .
+         bd:serviceParam mwapi:gcmprop "ids|title|type" .
+         bd:serviceParam mwapi:gcmlimit "max" .
+        ?item wikibase:apiOutputItem mwapi:item .
+      }}
+      ?item wdt:P31 wd:{type}
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+    }}""".format(category_name=category_name, type=type)
+
+    sparql.setQuery(a)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    res = [item['itemLabel']['value'] for item in results['results']['bindings']]
+    return res
 
 
 if __name__ == '__main__':
@@ -39,11 +64,15 @@ if __name__ == '__main__':
         for wikilist in tqdm.tqdm(lists):
             category_id = wikilist['category'].split('/')[-1]
             en_label = wikilist["category_wiki"].split('/')[-1]
+            type = wikilist["type"].split('/')[-1]
 
             try:
-                members = category_members(en_label)
+                members = category_members(en_label, type)
                 wikilist['members'] = members
-                writer.write(wikilist)
+                if members:
+                    writer.write(wikilist)
+                else:
+                    print(f'no members with type {type}: {en_label}')
             except KeyError as e:
                 print(e)
                 print(en_label, category_id)
