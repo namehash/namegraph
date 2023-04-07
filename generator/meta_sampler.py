66ff2bac9311328b437c1cb1214a78dc6ec8bac5
Byczong <piotrzwsln8@gmail.com>
2023-04-07 16:26:27 +0200

    add ens-norm check in meta sampler

--- generator/meta_sampler.py
+++ generator/meta_sampler.py
@@ -1,6 +1,7 @@
 import logging
 import random
 from typing import Type
+from ens_normalize import is_ens_normalized
 
 from generator.domains import Domains
 from generator.generated_name import GeneratedName
@@ -132,8 +133,8 @@ class MetaSampler:
                         suggestion.status = self.domains.get_name_status(str(suggestion))
                         # skip until it is not a duplicate and until it is "available" in case there are
                         # just enough free slots left to fulfill minimal available number of suggestions requirement
-
-                        while str(suggestion) in all_suggestions_str \
+                        while not is_ens_normalized(str(suggestion)) \
+                                or str(suggestion) in all_suggestions_str \
                                 or (suggestion.status != Domains.AVAILABLE
                                     and available_added + slots_left <= min_available_required):
                             suggestion = next(suggestions)
