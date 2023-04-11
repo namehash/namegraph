047228d9483337fb3d2a696fe952121307e2c2ee
Byczong <piotrzwsln8@gmail.com>
2023-04-11 12:23:48 +0200

    add classifier test

--- tests/test_person_names.py
+++ tests/test_person_names.py
@@ -3,12 +3,14 @@ from pytest import mark
 from hydra import initialize, compose
 
 from generator.utils.person_names import PersonNames
+from generator.input_name import InputName
+from generator.classifier.person_name_classifier import PersonNameClassifier
 
 
 @pytest.fixture(scope="module")
 def person_names():
     with initialize(version_base=None, config_path="../conf/"):
-        config = compose(config_name="test_config")
+        config = compose(config_name="test_config_new")
         pn = PersonNames(config)
         return pn
 
@@ -46,3 +48,30 @@ def test_person_names_benchmark(person_names, benchmark):
     assert not pn.tokenize('information')
 
     benchmark(pn.tokenize, 'the')
+
+
+def test_person_name_classifier_returns_language():
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="test_config_new")
+        pn_cls = PersonNameClassifier(config)
+
+        def classify_and_assert_lang(lang: str):
+            nonlocal name, pn_cls
+            name.strip_eth_namehash_unicode_replace_invalid = name.input_name
+            pn_cls.classify(name)
+            assert all([interpretations[0].lang == lang for interpretations in name.interpretations.values()])
+
+        name = InputName('kendrick', {})
+        classify_and_assert_lang('en')
+
+        name = InputName('małgorzata', {})
+        classify_and_assert_lang('pl')
+
+        name = InputName('noemie', {})
+        classify_and_assert_lang('fr')
+
+        name = InputName('alejandra', {})
+        classify_and_assert_lang('es')
+
+        name = InputName('boglarka', {})
+        classify_and_assert_lang('hu')
