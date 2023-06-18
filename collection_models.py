abea5fc374a5e75b879d4107f60ab3cecdb7f1e5
Krzysztof Wróbel <djstrong@gmail.com>
2023-06-18 18:10:37 +0200

    docs formatting

--- collection_models.py
+++ collection_models.py
@@ -3,27 +3,29 @@ from pydantic import BaseModel, Field
 
 
 class CollectionName(BaseModel):
-    name: str = Field(title='name with .eth')
+    name: str = Field(title='name with `.eth`')
     namehash: str = Field(title='namehash of the name')
 
 
 class Collection(BaseModel):
     collection_id: str = Field(title='id of the collection')
-    title: str = Field(title='title of the collections')
+    title: str = Field(title='title of the collection')
     owner: str = Field(title='ETH address of the collection owner')
     number_of_names: int = Field(title='total number of names in the collection')
-    last_updated_timestamp: int = Field(title='integer timestamp of last collection update')
+    last_updated_timestamp: int = Field(title='timestamp in milliseconds of last collection update')
     top_names: list[CollectionName] = Field(
-        title='top names stored in the collection (limited by limit_names)')
-    types: list[str] = Field(title='list of types to which the collection belongs')
+        title='top names stored in the collection (limited by `limit_names`)', description='can not be greater than 10')
+    types: list[str] = Field(title='list of types to which the collection belongs',
+                             description='example of type is `human`')
 
 
 class CollectionResultMetadata(BaseModel):
     total_number_of_matched_collections: Optional[int] = Field(
-        title='number of matched collections before trimming the result or "+1000" if more than 1000 results')  # TODO
+        title='number of matched collections before trimming the result or "+1000" if more than 1000 results',
+        description='return null for `count*` endpoints')  # TODO
     processing_time_ms: float = Field(title='time elapsed for this query in milliseconds')
     elasticsearch_processing_time_ms: Optional[float] = Field(
-        title='time elapsed for elasticsearch query in milliseconds')
+        title='time elapsed for elasticsearch query in milliseconds', description='return null for `count*` endpoints')
 
 
 class BaseCollectionQueryResponse(BaseModel):
@@ -35,14 +37,21 @@ class BaseCollectionQueryResponse(BaseModel):
 class BaseCollectionSearch(BaseModel):
     max_related_collections: int = Field(3, ge=0, title='max number of related collections to return')
     max_per_type: Optional[int] = Field(3,
-                                        title='number of collections with the same type which are not penalized; set to null if you want disable the penalization; if the penalization algorithm is turned on then 3 times more results (than max_related_collections) are retrieved from Elasticsearch')
+                                        title='number of collections with the same type which are not penalized',
+                                        description='* set to null if you want to disable the penalization\n'
+                                                    '* if the penalization algorithm is turned on then 3 times more results (than max_related_collections) are retrieved from Elasticsearch')
     name_diversity_ratio: Optional[float] = Field(
         0.5, ge=0.0, le=1.0,
-        title='similarity value used for adding penalty to collections with similar names to other collections; if more than name_diversity_ration of the names have already been used, penalize the collection; set to null if you want disable the penalization; ; if the penalization algorithm is turned on then 3 times more results (than max_related_collections) are retrieved from Elasticsearch'
+        title='similarity value used for adding penalty to collections with similar names to other collections',
+        description='* if more than name_diversity_ration of the names have already been used, penalize the collection\n'
+                    '* set to null if you want disable the penalization\n'
+                    '* if the penalization algorithm is turned on then 3 times more results (than max_related_collections) are retrieved from Elasticsearch'
     )
-    limit_names: int = Field(10, ge=0, le=10, title='the number of names returned in each collection')
+    limit_names: int = Field(10, ge=0, le=10, title='the number of names returned in each collection',
+                             description='can not be greater than 10')
     offset: int = Field(0,
-                        title='offset of the first collection to return (used for pagination); DO NOT use pagination with diversity algorithm')
+                        title='offset of the first collection to return (used for pagination)',
+                        description='DO NOT use pagination with diversity algorithm')
     sort_order: Literal['A-Z', 'Z-A', 'AI'] = Field('AI',
                                                     title='order of the resulting collections (by title for alphabetic sort)')
 
@@ -55,6 +64,7 @@ class BaseCollectionSearchWithOther(BaseCollectionSearch):  # instant search, do
 
 class CollectionSearchByString(BaseCollectionSearchWithOther):  # instant search, domain details
     query: str = Field(title='input query (with or without spaces) which is used to search for template collections',
+                       description='can not contain dots (.)',
                        regex=r'^[^\.]*$')
     mode: str = Field('instant', title='request mode: instant, domain_detail', regex=r'^(instant|domain_detail)$')
 
@@ -75,13 +85,13 @@ class CollectionsContainingNameCountRequest(BaseModel):
 
 
 class CollectionsContainingNameCountResponse(BaseCollectionQueryResponse):
-    count: int = Field(title='count of collections containing input name')
+    count: int = Field(title='count of collections containing input label')
 
 
 class CollectionsContainingNameRequest(BaseCollectionSearch):
     label: str = Field(title='label for which membership will be checked for each collection')
     mode: str = Field('instant', title='request mode: instant, domain_detail', regex=r'^(instant|domain_detail)$')
-    
+
 
 class CollectionsContainingNameResponse(BaseCollectionQueryResponse):
     collections: list[Collection] = Field(title='list of public collections the provided label is a member of')
