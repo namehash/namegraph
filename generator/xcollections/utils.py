77ea550d4b62a34c0bf2e0caeb0226a0642a9e1a
Byczong <piotrzwsln8@gmail.com>
2023-06-30 13:00:11 +0200

    fix namehash script field

--- generator/xcollections/utils.py
+++ generator/xcollections/utils.py
@@ -9,5 +9,5 @@ def get_names_script(limit_names=100) -> str:
 def get_namehashes_script(limit_names=100) -> str:
     return f"params['_source'].data.names.stream()" \
           + f".limit({limit_names})" \
-          + ".map(p -> p.normalized_name)" \
+          + ".map(p -> p.namehash)" \
           + ".collect(Collectors.toList())"
