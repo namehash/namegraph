f226e5cf7f875e9e8c9cfb42db0de5ea76b2285a
Krzysztof Wróbel <djstrong@gmail.com>
2023-08-23 00:53:27 +0200

    use spaces for tokenization in Collection Generator

--- tests/test_input_name.py
+++ tests/test_input_name.py
@@ -19,3 +19,31 @@ def test_do():
         for type, interpretations in name.interpretations.items():
             for interpretation in interpretations:
                 print(type, interpretation.tokenization, interpretation.in_type_probability, interpretation.features)
+
+
+def test_normalize():
+    with initialize(version_base=None, config_path="../conf/"):
+        config = compose(config_name="prod_config_new")
+        do = Preprocessor(config)
+
+        name = InputName('$zeuś.eth', {})
+        do.normalize(name)
+        assert name.strip_eth_namehash == '$zeuś'
+        assert name.strip_eth_namehash_unicode == '$zeus'
+        assert name.strip_eth_namehash_unicode_replace_invalid == 'zeus'
+
+        name = InputName('$zeuś' * 10, {})
+        do.normalize(name)
+        assert len(name.strip_eth_namehash_unicode_replace_invalid_long_name) == 30
+        assert name.strip_eth_namehash_unicode_replace_invalid_long_name == 'zeuszeuszeuszeuszeuszeuszeusze'
+        assert name.strip_eth_namehash_unicode_long_name == '$zeus$zeus$zeus$zeus$zeus$zeus'
+        assert name.strip_eth_namehash_long_name == '$zeuś$zeuś$zeuś$zeuś$zeuś$zeuś'
+
+        name = InputName('Zeus god', {})
+        do.normalize(name)
+        assert name.strip_eth_namehash == 'Zeus god'
+        assert name.strip_eth_namehash_unicode == 'zeus god'
+        assert name.strip_eth_namehash_unicode_replace_invalid == 'zeusgod'
+        assert name.strip_eth_namehash_unicode_replace_invalid_long_name == 'zeusgod'
+        assert name.strip_eth_namehash_unicode_long_name == 'zeus god'
+        assert name.strip_eth_namehash_long_name == 'Zeus god'
