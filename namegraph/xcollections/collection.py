b1940e0506cb2810d495eafa38cea481a9cf2378
djstrong <djstrong@gmail.com>
2025-01-14 15:34:34 +0100

    return avatar_emoji with in its text/monochrome form

--- namegraph/xcollections/collection.py
+++ namegraph/xcollections/collection.py
@@ -33,7 +33,7 @@ class Collection:
         self.tokenized_names = tokenized_names
         self.name_types = name_types
         self.modified_timestamp = modified_timestamp
-        self.avatar_emoji = avatar_emoji
+        self.avatar_emoji = avatar_emoji.replace('\ufe0f', '\ufe0e')
         self.avatar_image = avatar_image
         self.related_collections = related_collections
 
